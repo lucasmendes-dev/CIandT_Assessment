b3d3f7190d442b6219c0e516b1d6c0d7e0d2256b
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-05 17:37:26 -0300

    Challenge CI&T

--- challenge_9.php
+++ challenge_9.php
@@ -1,21 +1,34 @@
 <?php
 
+//Para este exercício foram usados valores da tabela ASCII, através da função "ord" do PHP.
 function menor_string_maior($name) {
 
-  $array = str_split($name);
+  if($name == ""){
 
-  $invertidas[] = array_pop($array);
-  $invertidas[] = array_pop($array);
-  
-  $array[] = $invertidas[0];
-  $array[] = $invertidas[1];
+    return "sem resposta";
+    
+  } else {
+    
+    $array = str_split($name);
 
-  return $array;
+    for($i = (sizeof($array) - 1); $i >= 1; $i--){  
+      if(ord($array[$i]) > ord($array[$i - 1])) {
+        $aux = $array[$i - 1];
+        $array[$i - 1] = $array[$i];
+        $array[$i] = $aux;
+        break;
+      } else if ($i == 1 && $array[$i] < $array[$i - 1]) {
+        return "sem resposta";
+      }
+    }
 
+
+    return implode("", $array);
+  }
 
 }
 
-$name = "abcde";
+$name = "Qualified";
 
 $result = menor_string_maior($name);
 print_r($result);
