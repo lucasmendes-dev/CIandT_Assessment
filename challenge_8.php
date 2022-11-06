ed5808b62733118bd8251cf0fba6b6498185a8cb
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-06 12:51:31 -0300

    Challenge CI&T

--- challenge_8.php
+++ challenge_8.php
@@ -1,15 +1,14 @@
 <?php
 
-function escolhe_taxi($tf1,$vqr1,$tf2,$vqr2) {
+    function escolhe_taxi($tf1,$vqr1,$tf2,$vqr2) {
 
-    $distancia1 = 15;
-    $distancia2 = 7;
+    $distancia1 = 15.0;
+    $distancia2 = 7.0;
 
     $tf1 = floatval($tf1);
     $vqr1 = floatval($vqr1);
     $tf2 = floatval($tf2);
     $vqr2 = floatval($vqr2);
-
 
     $empresa1_distancia1 = $tf1 + ($vqr1 * $distancia1);
     $empresa2_distancia1 = $tf2 + ($vqr2 * $distancia1);
@@ -24,23 +23,17 @@ function escolhe_taxi($tf1,$vqr1,$tf2,$vqr2) {
     } else if (($empresa1_distancia1 > $empresa2_distancia1) && ($empresa1_distancia2 > $empresa2_distancia2)) {
         return "Empresa 2";
     } else {
-        if(($empresa1_distancia1 == $empresa2_distancia1) && ($empresa1_distancia2 < $empresa2_distancia2)) {
-            return "Empresa 1 quando a distância < $distancia1.0, Tanto faz quando a distância = $distancia1.0, Empresa 2 quando a distância > $distancia1.0";
-        } else if (($empresa1_distancia1 == $empresa2_distancia1) && ($empresa1_distancia2 > $empresa2_distancia2)){
-            return "Empresa 1 quando a distância < $distancia1.0, Tanto faz quando a distância = $distancia1.0, Empresa 2 quando a distância > $distancia1.0";
-        } else if (($empresa1_distancia2 == $empresa2_distancia2) && ($empresa1_distancia1 < $empresa2_distancia1)){
-            return "Empresa 1 quando a distância < $distancia1.0, Tanto faz quando a distância = $distancia1.0, Empresa 2 quando a distância > $distancia1.0";
-        } else if (($empresa1_distancia2 == $empresa2_distancia2) && ($empresa1_distancia1 > $empresa2_distancia1)){
-            return "Empresa 1 quando a distância < $distancia2.0, Tanto faz quando a distância = $distancia2.0, Empresa 2 quando a distância > $distancia2.0";
-        } else {
-            if(($empresa1_distancia1 < $empresa2_distancia1) && ($empresa1_distancia2 > $empresa2_distancia2)){
-                return "Empresa 1 quando a distância < 10.0, Tanto faz quando a distância = 10.0, Empresa 2 quando a distância > 10.0";
-            } else if(($empresa1_distancia1 > $empresa2_distancia1) && ($empresa1_distancia2 < $empresa2_distancia2)) {
-                return "Empresa 1 quando a distância < 10.0, Tanto faz quando a distância = 10.0, Empresa 2 quando a distância > 10.0";
-            }
-        }
+    $fixa = $tf1 - $tf2;
+    $km = $vqr1 - $vqr2;
+    
+    $taxa_igual = abs($fixa / $km);
+    $taxa_igual = number_format((float)$taxa_igual, 2, '.');
+    if(fmod($taxa_igual, 1) == 0.00){
+        $taxa_igual = number_format((float)$taxa_igual, 1, '.');
+    } 
+    
+    return "Empresa 1 quando a distância < $taxa_igual, Tanto faz quando a distância = $taxa_igual, Empresa 2 quando a distância > $taxa_igual";
     }
-
 }
 
 
@@ -51,5 +44,3 @@ $vqr2 = '0.75';
 
 $empresa_escolhida = escolhe_taxi($tf1,$vqr1,$tf2,$vqr2);
 echo $empresa_escolhida;
-
-?>
--- rascunho.php
+++ rascunho.php
@@ -1,29 +1,37 @@
-function checa_numero_escondido($numero,$numeroOculto) {
-  
-  if(($numero == "" || $numeroOculto == "") || !str_contains($numero, $numeroOculto)){
-      return false;
-  } else {
-      $numero = str_split($numero);
-      $numero_verificacao = "";
+function escolhe_taxi($tf1,$vqr1,$tf2,$vqr2) {
 
-      for($i = 0; $i < sizeof($numero); $i++){
-          if(str_contains($numeroOculto, $numero[$i])){
-              $numero_verificacao .= "$numero[$i]";
-              if($numero_verificacao == $numeroOculto) {
-                  return true;
-              }
-          }
-      }
-      return false;
-  }  
-}
+$distancia1 = 15.0;
+$distancia2 = 7.0;
+$distancia_igual = number_format((float)10, 1, '.');
 
-$numero = '';
-$numeroOculto = '153';
+$tf1 = floatval($tf1);
+$vqr1 = floatval($vqr1);
+$tf2 = floatval($tf2);
+$vqr2 = floatval($vqr2);
 
-$checar = checa_numero_escondido($numero,$numeroOculto);
-if ($checar) {
-echo "O número $numero esconde o $numeroOculto. ";
+
+$empresa1_distancia1 = $tf1 + ($vqr1 * $distancia1);
+$empresa2_distancia1 = $tf2 + ($vqr2 * $distancia1);
+
+$empresa1_distancia2 = $tf1 + ($vqr1 * $distancia2);
+$empresa2_distancia2 = $tf2 + ($vqr2 * $distancia2);
+
+if(($empresa1_distancia1 == $empresa2_distancia1) && ($empresa1_distancia2 == $empresa2_distancia2)){
+    return "Tanto faz";
+} else if (($empresa1_distancia1 < $empresa2_distancia1) && ($empresa1_distancia2 < $empresa2_distancia2)) {
+    return "Empresa 1";
+} else if (($empresa1_distancia1 > $empresa2_distancia1) && ($empresa1_distancia2 > $empresa2_distancia2)) {
+    return "Empresa 2";
 } else {
-echo "O número $numero NÃO esconde o $numeroOculto. ";
+  return "Empresa 1 quando a distância < $distancia_igual, Tanto faz quando a distância = $distancia_igual, Empresa 2 quando a distância > $distancia_igual";
 }
+}
+
+
+$tf1 = '2.50';
+$vqr1 = '1.0';
+$tf2 = '5.0';
+$vqr2 = '0.75';
+
+$empresa_escolhida = escolhe_taxi($tf1,$vqr1,$tf2,$vqr2);
+echo $empresa_escolhida;
